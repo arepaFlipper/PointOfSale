3ba3ae116539211269980b552dda3319471cc9fe
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-10-18 12:43:01 -0500

    tthe page is nasty, but it's kind of the idea

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Categories from './Categories';
+import Carousel from './Carousel';
+import CarouselItem from './CarouselItem';
 import '../styles/components/Layout.styl';
 
 const Layout = ({ children }) => (
   <div className="Main">
     <Header />
+    <Categories>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
     {children}
     <Footer />
   </div>
