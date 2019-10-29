44de97687c8ce68daafa82182163103a149213f2
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-10-29 13:59:34 -0500

    UX enhanced

---
+++ src/containers/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
+import Layout from '../components/Layout';
+import Products from '../components/Products';
+
+const Home = () => (
+
+  <BrowserRouter>
+      <Switch>
+        <Layout>
+        <Route exact path="/home" component={Products} />
+        <Route exact path="/checkout" component={Checkout} />
+        </Layout>
+      </Switch>
+  </BrowserRouter>
+
+
+);
+
+export default Home;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,25 +9,20 @@ import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 
-import Home from '../containers/Home';
+import Home from '../containers/Dashboard';
 import Register from '../containers/Signup';
-import Checkout from '../containers/Checkout';
-import Layout from '../components/Layout';
 import NotFound from '../containers/NotFound';
 
 import '../assets/styles/App.scss';
 
 const App = () => (
   <BrowserRouter>
-    <Layout>
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/signup" component={Register} />
         <Route exact path="/home" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
         <Route component={NotFound} />
       </Switch>
-    </Layout>
   </BrowserRouter>
 );
 
