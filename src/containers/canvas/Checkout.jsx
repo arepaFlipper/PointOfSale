1596f67749c514b05207aee689f86ddc4b62f44b
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-11-11 18:59:09 -0500

    checkout enhanced

--- src/containers/canvas/Checkout.jsx
+++ src/containers/canvas/Checkout.jsx
@@ -11,13 +11,13 @@ const Checkout = (props) => {
         {cart.length > 0 ? <h3>Lista de Pedidos:</h3> : <h2>There is no order</h2>}
         {cart.map((item, index) => (
           <div className="Checkout-item" key={index}>
-            <div className="Checkout-element">
-              <h4>{item.product.title}</h4>
-              <span>$ {item.product.price}</span>
-              <span>x {item.amount}</span>
-              <span>($ {item.product.price * item.amount})</span>
+            <div className="Checkout-element"> 
+              <div className="Checkout-element_title"><h4>{item.product.title}</h4></div>
+              <div className="Checkout-element_container"><span>$ {item.product.price}</span></div>
+              <div className="Checkout-element_container"><span>x {item.amount}</span></div>
+              <div className="Checkout-element_container"><span>$ {item.product.price * item.amount}</span></div>
+              <i id="trash" className="fas fa-trash-alt" onClick={() => props.removeFromCart(item.idItemCart)} />
             </div>
-            <i className="fas fa-trash-alt" onClick={() => props.removeFromCart(item.idItemCart)} />
           </div>
         ))}
       </div>
@@ -27,9 +27,9 @@ const Checkout = (props) => {
           <h4>$ {`${cart.reduce(
             (accumulate, item) =>
             Math.ceil(accumulate + item.product.price * item.amount * 100), 0) / 100}`}</h4>
+          <button>Buy order</button>
         </div>
       )}
-      <button>Buy order</button>
     </div>
   )
 };
