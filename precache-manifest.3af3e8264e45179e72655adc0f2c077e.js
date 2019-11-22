6848d43edd9a180209fe692caa363516f27ad71a
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-11-22 03:13:40 -0500

    Updates

---
+++ precache-manifest.3af3e8264e45179e72655adc0f2c077e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8d77686020f3cd19f886e7b9e3fecd",
+    "url": "/PointOfSale/index.html"
+  },
+  {
+    "revision": "19004496ab87ab1868b0",
+    "url": "/PointOfSale/static/css/main.db27bb98.chunk.css"
+  },
+  {
+    "revision": "c39dfefaa9173aa6810c",
+    "url": "/PointOfSale/static/js/2.c2be88b7.chunk.js"
+  },
+  {
+    "revision": "19004496ab87ab1868b0",
+    "url": "/PointOfSale/static/js/main.362fb4b6.chunk.js"
+  },
+  {
+    "revision": "b1c8c2f87f340001d540",
+    "url": "/PointOfSale/static/js/runtime-main.41e7dab5.js"
+  }
+]);
